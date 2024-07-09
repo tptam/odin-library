218ce269ef77cc5b71185c97d87a93c1744a317c
tptam <99264397+tptam@users.noreply.github.com>
2024-07-10 03:30:12 +0900

    Add method to create book card

---
+++ script.js
@@ -0,0 +1,60 @@
+const myLibrary = [];
+
+function Book(title, author, page, read) {
+    this.title = title;
+    this.author = author;
+    this.page = page;
+    this.read = read;
+    this.info = () => {
+        let result = `${this.title} by ${thi.author}, ${this.page} pages, `;
+        result += this.read ? "already read" : "not read yet"
+        return result;
+    }
+}
+
+Book.prototype.getCard = function() {
+    const card = document.createElement("div");
+    const body = document.createElement("div");
+    const header = document.createElement("div");
+    const footer = document.createElement("div");
+    const title = document.createElement("h1");
+    const author = document.createElement("p");
+    const page = document.createElement("p");
+    const read = document.createElement("div");
+    card.classList.add("card");
+    body.classList.add("card-body");
+    header.classList.add("card-header");
+    footer.classList.add("card-footer");
+    title.classList.add("title");
+    author.classList.add("author");
+    page.classList.add("page");
+    read.classList.add("read", (this.read ? "true" : "false"));
+    title.textContent = this.title;
+    author.textContent = this.author;
+    page.textContent = this.page.toString();
+    read.textContent = this.read;
+    card.appendChild(header);
+    card.appendChild(body);
+    card.appendChild(footer);
+    header.appendChild(read);
+    body.appendChild(title);
+    body.appendChild(author);
+    footer.appendChild(page);
+    return card;
+}
+
+function addBookToLibrary() {
+}
+
+
+
+// const myFavorite = new Book(
+//     "10,000 Light-Years From Home",
+//     "James Tiptree Jr.",
+//     362,
+//     true
+// )
+
+// const body = document.querySelector("body");
+// const card = myFavorite.getCard();
+// body.appendChild(card);
